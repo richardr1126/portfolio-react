23d100dd87bfa7434f6b3e5c6a92e35b59638b5d
richardr1126 <richardr1126@gmail.com>
2024-09-15 14:51:20 -0600

    Update Sep 2024

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -22,8 +22,9 @@ function Experience({ id }) {
   return (
     <section id={id}>
       <Title order={2} mb={rem(15)}>Work Experience</Title>
-      <Paper shadow="sm" p={'md'} radius={'md'} withBorder>
 
+      {/* Existing Experience */}
+      <Paper shadow="sm" p={'md'} radius={'md'} withBorder>
         <Flex align={'center'} wrap={'wrap'} p={'xs'}>
           <ThemeIcon size={rem(100)} mb={'xs'} mr={'sm'} color="gray.1" radius={'lg'}>
             <Image src="cuplv.png" alt="UCB" width={rem(75)} height={rem(75)} />
@@ -32,7 +33,7 @@ function Experience({ id }) {
             <Stack spacing={0}>
               <Title order={3}>Machine Learning Research</Title>
               <Title order={4} color="dimmed">@ Programming Languages and Verification at the University of Colorado Boulder</Title>
-              <Text size="md" fw={600} color="dimmed">2023 - Present</Text>
+              <Text size="md" fw={600} color="dimmed">June 2023 - Febuary 2024</Text>
               <Space h={'sm'} />
               <Text size="lg" color="dimmed">
                 Leading a research project that finetunes large language models for text-to-SQL synthesis.
@@ -40,51 +41,55 @@ function Experience({ id }) {
               </Text>
               <Space h={'xs'} />
               <Group spacing={rem(5)}>
-                <Badge color="teal" variant="light" size="lg">
-                  Python
-                </Badge>
-                <Badge color="indigo" variant="light" size="lg">
-                  C++
-                </Badge>
-                <Badge color="primary" variant="light" size="lg">
-                  LLM Finetuning
-                </Badge>
+                <Badge color="teal" variant="light" size="lg">Python</Badge>
+                <Badge color="indigo" variant="light" size="lg">C++</Badge>
+                <Badge color="primary" variant="light" size="lg">LLM Finetuning</Badge>
               </Group>
               <Space h={'xs'} />
             </Stack>
           </MediaQuery>
-
         </Flex>
-        <Group mt={'xs'} spacing={rem(5)} style={{flex: 3}}>
-          <Button
-            h={rem(35)}
-            variant='light'
-            color="teal"
-            leftIcon={<FaSearch />}
-            style={{ flex: 1 }}
-            onClick={(e) => scrollTo(e, '#projects')}>
+        <Group mt={'xs'} spacing={rem(5)} style={{ flex: 3 }}>
+          <Button h={rem(35)} variant='light' color="teal" leftIcon={<FaSearch />} style={{ flex: 1 }} onClick={(e) => scrollTo(e, '#projects')}>
             Learn more about my project
           </Button>
-          <Button
-            h={rem(35)}
-            variant='light'
-            leftIcon={<FaGithub />}
-            style={{ flex: 1 }}
-            onClick={() => window.open('https://github.com/cuplv', '_blank')}>
+          <Button h={rem(35)} variant='light' leftIcon={<FaGithub />} style={{ flex: 1 }} onClick={() => window.open('https://github.com/cuplv', '_blank')}>
             CUPLV GitHub
           </Button>
-          <Button
-            h={rem(35)}
-            variant='light'
-            color='blue'
-            leftIcon={<MdWeb />}
-            style={{ flex: 1 }}
-            onClick={() => window.open('https://plv.colorado.edu/', '_blank')}>
+          <Button h={rem(35)} variant='light' color='blue' leftIcon={<MdWeb />} style={{ flex: 1 }} onClick={() => window.open('https://plv.colorado.edu/', '_blank')}>
             CUPLV Website
           </Button>
         </Group>
+      </Paper>
 
-
+      {/* New UpWork Experience */}
+      <Paper shadow="sm" p={'md'} radius={'md'} withBorder mt={'md'}>
+        <Flex align={'center'} wrap={'wrap'} p={'xs'}>
+          <MediaQuery largerThan={600} styles={{ flex: 1, marginLeft: rem(10) }}>
+            <Stack spacing={0}>
+              <Title order={3}>Freelance Front-End Developer</Title>
+              <Text size="md" fw={600} color="dimmed">June 2024 - Present</Text>
+              <Space h={'sm'} />
+              <Text size="md" color="dimmed">
+                "Richard was able to quickly complete the tasks assigned to him, along with finding and fixing a few issues he found on his own. He quickly was able to understand what was being asked and was able to take initiative and perform his tasks despite our somewhat half-baked and incomplete requirements. He will be one of the first places we look for front-end development work from now on."
+                - Client Feedback
+              </Text>
+              <Space h={'sm'} />
+              <Group spacing={rem(5)}>
+                <Badge color="teal" variant="light" size="lg">React</Badge>
+                <Badge color="orange" variant="light" size="lg">Front-End Development</Badge>
+              </Group>
+              <Space h={'xs'} />
+            </Stack>
+          </MediaQuery>
+        </Flex>
+        <Flex mt={'xs'}>
+          <Button h={rem(35)} variant='light' color="teal" leftIcon={<FaSearch />} style={{ flex: 1 }} onClick={(e) =>  /* Add link to website */ {
+            window.open('https://www.upwork.com/freelancers/~015b295507f2496334', '_blank');
+          }}>
+            Visit freelance profile
+          </Button>
+        </Flex>
       </Paper>
     </section>
   );
